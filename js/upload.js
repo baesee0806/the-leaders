e3b47ab27840b4f7c6970792409b25e68cf85302
heegeer <117061017+heegeer@users.noreply.github.com>
2022-11-24 11:26:31 +0900

    Update upload.js
    이미지 업로드 기능 제외 글 작성 완료해서 Firestore에 저장함

--- js/upload.js
+++ js/upload.js
@@ -1,52 +1,50 @@
-
-// < !--JavaScript Bundle with Popper-- >
-
+import { addDoc, collection,} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+import { dbService } from "./firebase.js";
 
 // const db = firebase.firestore();
-// // const storage = firebase.storage();
-const storageRef = firebase.storage().ref();
-export function uploading() {$('#send').click(() => {
-    var file = document.querySelector('#image').files[0];
-    var 저장할경로 = storageRef.child('image/' + file.name);
-    var 업로드작업 = 저장할경로.put(file)
+// const storage = firebase.storage();
+// const storageRef = firebase.storage().ref();
+
+export const uploading = async (event) => {
+    // event.preventDefault();
+    // textarea 태그에 연결된 id 값이 "comment"
+    // const post = document.getElementById("comment");
+    // const { uid, photoURL, displayName } = authService.currentUser;
+    
+    // var file = document.querySelector('#image').files[0];
+    // var 저장할경로 = storageRef.child('image/' + file.name);
+    // var 업로드작업 = 저장할경로.put(file)
+
     var today = new Date();
     var year = today.getFullYear();
     var month = ('0' + (today.getMonth() + 1)).slice(-2);
     var day = ('0' + today.getDate()).slice(-2);
     var dateString = year + '-' + month + '-' + day;
 
-    업로드작업.on('state_changed',
-        // 변화시 동작하는 함수 
-        null,
-        //에러시 동작하는 함수
-        (error) => {
-            console.error('실패사유는', error);
-        },
-        // 성공시 동작하는 함수
-        () => {
-            업로드작업.snapshot.ref.getDownloadURL().then((url) => {
-                console.log('업로드된 경로는', url);
-                const 저장할거 = {
-                    분류: $('#category').val(),
-                    제목: $('#title').val(),
-                    평점: parseInt($('#score').val()),
-                    내용: $('#content').val(),
-                    작성일: dateString,
-                    이미지: url,
-                    // uid: JSON.parse(localStorage.getItem('user')).uid,
-                    // 이름: JSON.parse(localStorage.getItem('user')).displayName
-                }
-                db.collection('product').add(저장할거).then(() => {
-                    alert('저장완료');
-                    window.location.href = '/index.html'
-                }).catch((err) => {
-                    alert(err);
-                    window.location.href = '/index.html'
-                })
+        
+    
+    try {
+      // addDoc(DB에 저장) API 이용해서 "product" collection에 
+      // 다섯 개의 속성(text, nickname...)을 가진 객채를 하나의 Document로 신규 등록함
+      await addDoc(collection(dbService, "product"), {
+        분류: $('#category').val(),
+        제목: $('#title').val(),
+        평점: parseInt($('#score').val()),
+        내용: $('#content').val(),
+        작성일: dateString,
+        // 이미지: 
+       });
+        
+       alert('저장완료');
 
-            });
-        }
-    );
+        // 유저 본인의 게시물에만 수정, 삭제 버튼을 나타내기 위해서 uid 값이 필요함
+    //     creatorId: uid,
+    //     profileImg: photoURL,
+    //     nickname: displayName,
 
+    } catch (error) {
+         alert(error);
+         console.log("error in addDoc:", error);
+    };
 
-})}
+};
