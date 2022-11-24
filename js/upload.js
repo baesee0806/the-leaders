cb189a1c856f0708420d6e0146e839db86393b0f
heeryoung <winterbooks@naver.com>
2022-11-24 20:49:34 +0900

    컬렉션명 post로 수정, 필드에 이메일 추가

--- js/upload.js
+++ js/upload.js
@@ -1,6 +1,5 @@
-import { addDoc, setDoc, doc, collection } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+import { addDoc, collection } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
 import { dbService, authService } from "./firebase.js";
-
 
 export const uploading = async (event) => {
     event.preventDefault();
@@ -8,7 +7,7 @@ export const uploading = async (event) => {
     const title = document.getElementById("title")
     const content = document.getElementById("content");
     const score = document.getElementById("score")
-    // const { uid, photoURL, displayName } = authService.currnetUser.uid
+    const { uid, email, displayName } = authService.currentUser;
 
     const today = new Date();
     const year = today.getFullYear();
@@ -17,15 +16,16 @@ export const uploading = async (event) => {
     const dateString = year + '-' + month + '-' + day;
 
     try {
-        await addDoc(collection(dbService, "upload"), {
+        await addDoc(collection(dbService, "post"), {
             카테고리: category.value,
             제목: title.value,
             내용: content.value,
             평점: parseInt(score.value),
             작성일: dateString,
-            // 작성자: uid
+            이메일: email,
         });
         alert("등록완료");
+        console.log(authService)
     } catch (error) {
         alert(error);
         console.log("error in addDoc:", error);
